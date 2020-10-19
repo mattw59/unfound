7fb57f6634ca580399f392c864f2fb4cda62d309
Matt <matt@mattwhite.co>
2020-10-19 23:40:31 +0000

    added some rudimentary sketches of how to lay out hexagons in a tessellation

--- src/game.ts
+++ src/game.ts
@@ -165,3 +165,46 @@ function draw() {
 }
 
 draw();
+
+// var ctx = document.getElementById('hexagon').getContext('2d');
+
+// // hexagon
+// var numberOfSides = 6,
+//     size = 20,
+//     Xcenter = 25,
+//     Ycenter = 25;
+	
+// //column 1, row 1
+// drawPolygon(numberOfSides, size, Xcenter, Ycenter);
+// column 1, row 2
+// drawPolygon(numberOfSides, size, Xcenter, Ycenter+Math.sqrt(3)*size);
+// column 1, row 3
+// drawPolygon(numberOfSides, size, Xcenter, Ycenter+2*Math.sqrt(3)*size);
+
+// //column 2, row 1
+// drawPolygon(numberOfSides, size, Xcenter+3*size/2, Ycenter+Math.sqrt(3)*size/2);
+// column 2, row 2
+// drawPolygon(numberOfSides, size, Xcenter+3*size/2, Ycenter+3*Math.sqrt(3)*size/2);
+
+// //column 3, row 1
+// drawPolygon(numberOfSides, size, Xcenter + 3*size, Ycenter);
+// column 3, row 2
+// drawPolygon(numberOfSides, size, Xcenter + 3*size, Ycenter+Math.sqrt(3)*size);
+// column 3, row 3
+// drawPolygon(numberOfSides, size, Xcenter+3*size, Ycenter+2*Math.sqrt(3)*size);
+
+
+// function drawPolygon(numberOfSides, size, Xcenter, Ycenter) {
+// ctx.beginPath();
+// ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
+
+// for (var i = 1; i <= numberOfSides;i += 1) {
+//   ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
+// }
+
+// ctx.strokeStyle = "#000000";
+// ctx.lineWidth = 1;
+// ctx.stroke();
+
+// }
+
